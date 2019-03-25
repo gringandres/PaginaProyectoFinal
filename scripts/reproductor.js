6e77f6092168b74a0c4be563f803e29a59733dc3
Gringo <trand0161@gmail.com>
2019-03-24 19:40:32 -0500

    Big change
    organizo btn y js

--- scripts/reproductor.js
+++ scripts/reproductor.js
@@ -1,2 +1,53 @@
+document.getElementById("btn1").addEventListener("click", function(){
+    document.getElementById("music1").hidden = false;
+    document.getElementById("music2").hidden = true;
+    document.getElementById("music3").hidden = true;
+    document.getElementById("music4").hidden = true;
+    document.getElementById("music5").hidden = true;
+    document.getElementById("music6").hidden = true;
+});
 
-let tracklist = document.querySelectorAll('.music');
+document.getElementById("btn2").addEventListener("click", function(){
+    document.getElementById("music1").hidden = true;
+    document.getElementById("music2").hidden = false;
+    document.getElementById("music3").hidden = true;
+    document.getElementById("music4").hidden = true;
+    document.getElementById("music5").hidden = true;
+    document.getElementById("music6").hidden = true;
+});
+
+document.getElementById("btn3").addEventListener("click", function(){
+    document.getElementById("music1").hidden = true;
+    document.getElementById("music2").hidden = true;
+    document.getElementById("music3").hidden = false;
+    document.getElementById("music4").hidden = true;
+    document.getElementById("music5").hidden = true;
+    document.getElementById("music6").hidden = true;
+});
+
+document.getElementById("btn4").addEventListener("click", function(){
+    document.getElementById("music1").hidden = true;
+    document.getElementById("music2").hidden = true;
+    document.getElementById("music3").hidden = true;
+    document.getElementById("music4").hidden = false;
+    document.getElementById("music5").hidden = true;
+    document.getElementById("music6").hidden = true;
+});
+
+document.getElementById("btn5").addEventListener("click", function(){
+    document.getElementById("music1").hidden = true;
+    document.getElementById("music2").hidden = true;
+    document.getElementById("music3").hidden = true;
+    document.getElementById("music4").hidden = true;
+    document.getElementById("music5").hidden = false;
+    document.getElementById("music6").hidden = true;
+});
+
+document.getElementById("btn6").addEventListener("click", function(){
+    document.getElementById("music1").hidden = true;
+    document.getElementById("music2").hidden = true;
+    document.getElementById("music3").hidden = true;
+    document.getElementById("music4").hidden = true;
+    document.getElementById("music5").hidden = true;
+    document.getElementById("music6").hidden = false;
+});
