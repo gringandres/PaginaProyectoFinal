b08089e7733b407d997395516e9ec79a188454ef
Gringo <trand0161@gmail.com>
2019-05-22 15:05:11 -0500

    .

---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+function myFunction3() {
+    var nv = window.outerHeight;
+
+    if (nv > 700) {
+        document.getElementById("navBar").style.backgroundColor = "transparent";
+    }
+    else{
+        document.getElementById("navBar").style.backgroundColor = "white";
+    }
+
+}
+
